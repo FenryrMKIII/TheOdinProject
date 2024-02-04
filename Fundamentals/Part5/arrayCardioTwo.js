094690bddda75d598809e880544ecb9116733460
FenryrMKIII <FenryrMKIII@FenryrMKIII>
2024-02-04 21:55:55 +0100

    feat: new arrayCardioTwo lesson added

---
+++ Fundamentals/Part5/arrayCardioTwo.js
@@ -0,0 +1,19 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Array.prototype.some() // is at least one person 19 or older?
+console.log(people.some(people => 2024-people.year>=19));
+
+console.log(people.every(people => 2024-people.year>=19));
